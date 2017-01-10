73c655b0ce8dcc62ea5b3c156003182f059249ea
liudg <liudg95@gmail.com>
2017-01-10 17:56:21 +0800

    修改

--- app/src/main/java/com/liudong/douban/data/DataManager.java
+++ app/src/main/java/com/liudong/douban/data/DataManager.java
@@ -27,6 +27,7 @@ public class DataManager {
         mDouBanService = douBanService;
         mDataBaseHelper = dataBaseHelper;
         mPreferencesHelper = preferencesHelper;
+        System.out.println("1111");
     }
 
     public PreferencesHelper getPreferencesHelper() {
